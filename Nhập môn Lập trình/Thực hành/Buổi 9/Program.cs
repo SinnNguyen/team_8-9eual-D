8cebe82e6aa0a21d2648e8164d141e9e4472e571
SinnNguyen <sinndeptrai0123@gmail.com>
2021-12-31 15:11:33 +0700

    Update Program.cs

--- Nhập môn Lập trình/Thực hành/Buổi 9/Program.cs
+++ Nhập môn Lập trình/Thực hành/Buổi 9/Program.cs
@@ -5,7 +5,7 @@ namespace Buoi_9
 {
     internal class Program
     {
-        
+
         static int Menu()
         {
             Console.Clear();
@@ -20,11 +20,11 @@ namespace Buoi_9
             Console.WriteLine(new string('=', 60));
             Console.WriteLine();
             Console.Write("     Nhap lua chon: "); int x = int.Parse(Console.ReadLine());
-                return x;
+            return x;
         }
         static void DemKyTu(string filePath)
         {
-            if (File.Exists(filePath)) 
+            if (File.Exists(filePath))
             {
                 string[] lines = File.ReadAllLines(filePath);
 
@@ -52,35 +52,86 @@ namespace Buoi_9
             else
                 Console.WriteLine("     Khong tim thay file");
         }
+        static void RobotDTNB(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                string[] s = lines[0].Split();
+                int X = int.Parse(s[0]); int Y = int.Parse(s[1]);
+                string[] move = new string[lines[1].Length + 1];
+                move[0] = lines[0];
+                Console.WriteLine("       Begin : " + move[0]);
+                for (int i = 0; i < lines[1].Length; i++)
+                {
+
+                    switch (lines[1][i])
+                    {
+                        case 'B': { Y = Y + 1; break; }
+                        case 'D': { X = X + 1; break; }
+                        case 'N': { Y = Y - 1; break; }
+                        case 'T': { X = X - 1; break; }
+                    }
+                    move[i + 1] = X.ToString() + " " + Y.ToString();
+                    Console.WriteLine($"Step {i + 1}  {lines[1][i]}  :  {move[i + 1]}");
+                }
+                File.WriteAllLines("../../../TextFile/Moves.txt", move);
+            }
+            else
+            {
+                Console.WriteLine(" Khong tim thay file");
+            }
+        }
+        static string CongHaiSoNguyenLon(string x, string y)
+        {
+            int len = x.Length > y.Length ? x.Length : y.Length;
+            string kq = "";
+            if (x.Length < len)
+                x = "0" + x;
+            if (y.Length < len)
+                y = "0" + y;
+            int nho = 0;
+            for (int i = len - 1; i >= 0; i--)
+            {
+                int cong = int.Parse(x[i].ToString()) + int.Parse(y[i].ToString()) + nho;
+                kq = (cong % 10).ToString() + kq;
+                nho = cong / 10;
+            }
+            Console.WriteLine($"ket qua : {kq}");
+            return kq;
+        }
         static void Main(string[] args)
-        {           
+        {
             int chon = Menu();
             switch (chon)
             {
                 case 1:
                     {
                         string filePath = "../../../TextFile/Input.txt";
-                        DemKyTu(filePath); 
+                        DemKyTu(filePath);
                     }
                     break;
                 case 2:
                     {
-
+                        ;
                     }
                     break;
                 case 3:
                     {
-                        
+                        ;
                     }
                     break;
                 case 4:
                     {
-                       
+                        string filePath = "../../../TextFile/Direction.Txt";
+                        RobotDTNB(filePath);
                     }
                     break;
                 case 5:
                     {
-                        
+                        Console.Write("Nhap chuoi x = "); string x = Console.ReadLine();
+                        Console.Write("Nhap chuoi y = "); string y = Console.ReadLine();
+                        CongHaiSoNguyenLon(x, y);
                     }
                     break;
                 default:
